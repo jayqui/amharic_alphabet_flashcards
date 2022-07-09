12faeebc596c2857974422bacf30bfa1efd04ae9
Jay Quigley <quigley.james.g@gmail.com>
2022-07-09 13:12:30 -0700

    Basic setup: ability to scroll through the fidel

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import fidel from './fidel.json';
 
 export default function App() {
+  const [displayOptionIndex, setDisplayOptionIndex] = useState(0);
+  const [shouldShowHelp, setShouldShowHelp] = useState(true);
+
+  function handlePreviousPress() {
+    console.log('new Previous button press at', new Date())
+    const newIndex = displayOptionIndex > 0 ? displayOptionIndex - 1 : displayOptionIndex;
+    setDisplayOptionIndex(newIndex)
+  }
+
+  function handleNextPress() {
+    console.log('new Next button press at', new Date())
+    const newIndex = displayOptionIndex < fidel.length - 1 ? displayOptionIndex + 1 : displayOptionIndex;
+    console.log('newIndex', newIndex)
+    setDisplayOptionIndex(newIndex)
+  }
+
+  const currentLetter = fidel[displayOptionIndex];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={[styles.text, styles.character]}>{currentLetter.character}</Text>
+      <Text style={[styles.text, styles.transliteration]}>{shouldShowHelp ? currentLetter.transliteration : "_"}</Text>
       <StatusBar style="auto" />
+      <View style={styles.buttonArea}>
+        <Button title="❌" onPress={handlePreviousPress}></Button>
+        <Button title="✅" onPress={handleNextPress}></Button>
+      </View>
+      <Button title="Toggle Help" onPress={() => setShouldShowHelp(!shouldShowHelp)}></Button>
     </View>
   );
 }
@@ -13,8 +40,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+  },
+  buttonArea: {
+    backgroundColor: '#fff',
+    height: 100,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  yesButton: {
+    height: 100,
+    width: '50%',
+    backgroundColor: '#0f8',
+  },
+  text: {
+    color: '#ff0000',
+  },
+  character: {
+    fontSize: 100,
+  },
+  transliteration: {
+    fontSize: 50,
   },
 });
