9bfaec2d088247c8af8960ca9d2afbbe37351076
Jay Quigley <quigley.james.g@gmail.com>
2022-07-09 14:19:11 -0700

    Random choice of 3 letters from the fidel
    * Delay + show after x button is pressed
    * Restart button

--- App.tsx
+++ App.tsx
@@ -1,37 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { sample, sampleSize } from 'lodash';
 
 import fidel from './fidel.json';
 
 export default function App() {
-  const [displayOptionIndex, setDisplayOptionIndex] = useState(0);
-  const [shouldShowHelp, setShouldShowHelp] = useState(true);
+  const [shouldShowHelp, setShouldShowHelp] = useState(false);
+  const [queue, setQueue] = useState(generateFidelSample());
+  const [currentLetter, setCurrentLetter] = useState(sample(queue));
 
-  function handlePreviousPress() {
-    console.log('new Previous button press at', new Date())
-    const newIndex = displayOptionIndex > 0 ? displayOptionIndex - 1 : displayOptionIndex;
-    setDisplayOptionIndex(newIndex)
+  console.log('queue.length', queue.length)
+  console.log('queue:', queue.map((x) => x.character));
+
+  function generateFidelSample() {
+    return sampleSize(fidel, 3);
   }
 
-  function handleNextPress() {
-    console.log('new Next button press at', new Date())
-    const newIndex = displayOptionIndex < fidel.length - 1 ? displayOptionIndex + 1 : displayOptionIndex;
-    console.log('newIndex', newIndex)
-    setDisplayOptionIndex(newIndex)
+  function handleXPress() {
+    console.log(`❌ was pressed for ${currentLetter?.character}`, new Date())
+
+    setShouldShowHelp(true);
+    setTimeout(() => {
+      setShouldShowHelp(false);
+      setCurrentLetter(sample(queue));
+    }, 1000)
   }
 
-  const currentLetter = fidel[displayOptionIndex];
+  function handleCheckPress() {
+    console.log(`✅ was pressed for ${currentLetter?.character}`, new Date())
+
+    const newQueue = queue.filter((ele: { character: string; }) =>
+      ele.character !== currentLetter?.character);
+
+    setQueue(newQueue);
+    setCurrentLetter(sample(queue));
+  }
+
+  function handleRestartPress() {
+    const newQueue = generateFidelSample();
+    setQueue(newQueue);
+    setCurrentLetter(sample(newQueue));
+  }
+
+  function renderSuccess() {
+    return (
+      <>
+        <Text>Yay!</Text>
+        <Button title="Restart" onPress={handleRestartPress}></Button>
+      </>
+    )
+  }
+
+  function renderStuff() {
+    return(
+      <>
+        <Text style={[styles.text, styles.character]}>{currentLetter?.character}</Text>
+        <Text style={[styles.text, styles.transliteration]}>
+          {shouldShowHelp ? currentLetter?.transliteration : "_"}
+        </Text>
+      </>
+    )
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={[styles.text, styles.character]}>{currentLetter.character}</Text>
-      <Text style={[styles.text, styles.transliteration]}>{shouldShowHelp ? currentLetter.transliteration : "_"}</Text>
+      {queue.length ? renderStuff() : renderSuccess()}
       <StatusBar style="auto" />
-      <View style={styles.buttonArea}>
-        <Button title="❌" onPress={handlePreviousPress}></Button>
-        <Button title="✅" onPress={handleNextPress}></Button>
-      </View>
+      {queue.length > 0 && <View style={styles.buttonArea}>
+        <Button title="❌" onPress={handleXPress}></Button>
+        <Button title="✅" onPress={handleCheckPress}></Button>
+      </View>}
       <Button title="Toggle Help" onPress={() => setShouldShowHelp(!shouldShowHelp)}></Button>
     </View>
   );
