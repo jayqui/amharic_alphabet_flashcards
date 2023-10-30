93751b3f926cb612c980c8a1f3d3d709c665ea96
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2023-10-30 14:06:04 -0700

    change test assertion to something more helpful; we can now use this to begin test-driving out the new stats screen

--- app/components/MainMenu.unit.test.tsx
+++ app/components/MainMenu.unit.test.tsx
@@ -1,8 +1,11 @@
 import renderer from "react-test-renderer"
 import MainMenu from "./MainMenu"
+import { Button } from "react-native-paper"
 
 describe("MainMenu", () => {
-    it("basically renders", () => {
-        expect(renderer.create(<MainMenu navigate={() => {}}/>)).toBeTruthy();
+    it("includes two buttons", () => {
+        const mockNavigate = () => {};
+        const testInstance = renderer.create(<MainMenu navigate={mockNavigate}/>).root;
+        expect(testInstance.findAllByType(Button).length).toBe(2);
     });
 });
